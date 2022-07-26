16a8fa750e0644f6947134c9e5679c6d16593a5f
Jemerson Maia <jemerson.maia@asaas.com.br>
2022-07-25 21:59:54 -0300

    Removendo pipe

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,12 @@ java -jar /lib/codenarc-all.jar \
     -rulesetfiles="${INPUT_RULESETFILES}" \
     > result.txt || exit 1
 
-< result.txt \
-  | reviewdog -efm="%f:%l:%m" -efm="%f:%r:%m" \
-      -name="codenarc" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      "${INPUT_REVIEWDOG_FLAGS}"
+< result.txt reviewdog -efm="%f:%l:%m" -efm="%f:%r:%m" \
+    -name="codenarc" \
+    -reporter="${INPUT_REPORTER:-github-pr-check}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    "${INPUT_REVIEWDOG_FLAGS}"
 
 #rm result.txt
